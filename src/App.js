b408515d8d0dfff434fe796045c1cd00e3c25c63
AYZLFC <almogzamir@gmail.com>
2022-11-26 13:18:30 +0200

    Move delta to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-
 import './App.css';
 import Counter from './Counter';
+import { useState } from "react";
 
 function App() {
+  const [delta, setDelta] = useState(1)
+  function handleDelta(event){
+    //console.log(event);
+    setDelta(Number(event.target.value))
+  }
   return (
     <div className="App">
       Usestate
-      
-      <Counter />
+      <br/>
+      <input type= "number" value={delta} onChange={handleDelta}/>
+      <Counter delta = {delta}  />
+      <Counter delta = {delta} />
 
     </div>
   );
