7c21fcb0bde2342dddac560dc7fb9d552a8232d0
AYZLFC <almogzamir@gmail.com>
2022-11-27 11:38:14 +0200

    Props training

--- src/Counter.js
+++ src/Counter.js
@@ -2,15 +2,16 @@
 import './Counter.css';
 import { useState } from "react";
 function Counter(props) {
+    const {delta} = props
     const [count, setCount] = useState(1)
 
     function incr(){
         setCount(
             function(oldCount){
-                if (oldCount + props.delta > props.maxNum){
+                if (oldCount + delta > props.maxNum){
                   return 1
                 }
-                return oldCount + props.delta
+                return oldCount + delta
             }
         )
     }
@@ -26,9 +27,8 @@ function Counter(props) {
     return (
       <div className="Counter" >
         <h1>Counter</h1>
-        
         <p>Counter is at {count}</p>
-        <button onClick={incr}>Click to add {props.delta} to Counter</button>
+        <button onClick={incr}>Click to add {delta} to Counter</button>
         <button onClick={reset}>Click to reset the Counter</button>
 
       </div>
